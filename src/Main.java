7ee2dcdf021328c6805964d7895b875aaa369e25
Josue Cano <jdevelopcano@gmail.com>
2025-03-12 12:32:57 -0600

    added methods getArea, getDescuento, getIVA, getPrecioWithIVA

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,15 @@ public class Main {
         ejemplo.oddEven(200);
         ejemplo.oddEven(50);
         System.out.println(ejemplo.getFactorial(3));
+        System.out.println(ejemplo.getIVA(200));
+        System.out.println("precio +  iva");
+        System.out.println(ejemplo.getPrecioWithIVA(200));
         //usando metodo estatico
         Ejemplos.greetings();
         Ejemplos.getChessBoard(10);
-
+        System.out.println("Area de un triangulo");
+        System.out.println(Ejemplos.getArea(5,3));
+        System.out.println(Ejemplos.getDescuento(-100));
         int suma = sumar(2,4);
         System.out.println(suma);
 
--- src/com/generation/metodos/Ejemplos.java
+++ src/com/generation/metodos/Ejemplos.java
@@ -29,6 +29,17 @@ public class Ejemplos {
         return factorial;
     }
 
+    public double getIVA(int precio){
+        double iva = precio * 0.16;
+        return iva;
+    }
+
+    public double getPrecioWithIVA(int precio){
+        double iva = getIVA(precio);
+        double precioTotal = precio + iva;
+        return precioTotal;
+    }
+
     //metodo estatico
     public static void greetings(){
         System.out.println("Saludos");
@@ -40,6 +51,28 @@ public class Ejemplos {
                 System.out.print("* ");
             }
             System.out.println();
+        }
+    }
+
+    public static float getArea(int base, int height){
+        int basePlusHeight = base * height;
+        float area = basePlusHeight / 2.0f;
+        return area;
+    }
+
+    /*
+    * 1. de 100 a  500 -> 10% descuento
+    * 2. 501  a 1000 ->  15%
+    * 3. 1001 a infinito -> 20%
+    * */
+
+    public static float getDescuento(int consumo){
+        if(consumo < 100)return consumo;
+        if(consumo >= 100 && consumo <= 500)return consumo * 0.10f;
+        if(consumo >= 501 && consumo <= 1000){
+            return consumo *  0.15f;
+        }else{
+            return consumo * 0.20f;
         }
     }
 }
